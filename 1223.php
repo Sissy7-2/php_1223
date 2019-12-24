3b6609a5a2a4b1e62c257ec3fb9232061c4291e4
名前 <>
2019-12-24 03:54:47 +0000

    kadai_second

---
+++ 1223.php
@@ -0,0 +1,42 @@
+<?php
+
+//【課題1】
+/*
+$name = "marina";
+
+if ($name == "marina") {
+    echo "私は　marina　です";
+} else {
+    echo "marinaではありません";
+}
+*/
+
+//【課題2】
+/*$total = 10000;
+echo $total;
+for ($i = 0; $i <= 10000; $i++) {
+    $total += $i;
+}
+echo $total;
+*/
+
+//【課題3】
+/*
+$fruits = array("orange", "pineapple", "mango", "mulberry", "persimmon");
+foreach($fruits as $fruit){
+    echo "要素は" . $fruit;
+    echo "\n";
+}
+*/
+//【4.応用】
+/*
+$start = 1;
+$end = 100;
+
+for($i = $start; $i <= $end; $i++){
+
+  if($i % 5 == 0){
+    echo $i;
+  }
+}
+*/
