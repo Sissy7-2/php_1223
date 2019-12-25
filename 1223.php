8858305e9eab80894e226a348e2f617926702227
名前 <>
2019-12-25 05:13:38 +0000

    re_kadai_second

--- 1223.php
+++ 1223.php
@@ -1,7 +1,7 @@
 <?php
 
 //【課題1】
-/*
+
 $name = "marina";
 
 if ($name == "marina") {
@@ -9,27 +9,30 @@ if ($name == "marina") {
 } else {
     echo "marinaではありません";
 }
-*/
+
+echo "\n";
 
 //【課題2】
-/*$total = 10000;
+$total = 0;
 echo $total;
 for ($i = 0; $i <= 10000; $i++) {
     $total += $i;
 }
 echo $total;
-*/
+
+echo "\n";
 
 //【課題3】
-/*
+
 $fruits = array("orange", "pineapple", "mango", "mulberry", "persimmon");
 foreach($fruits as $fruit){
     echo "要素は" . $fruit;
     echo "\n";
 }
-*/
+
+
 //【4.応用】
-/*
+
 $start = 1;
 $end = 100;
 
@@ -37,6 +40,8 @@ for($i = $start; $i <= $end; $i++){
 
   if($i % 5 == 0){
     echo $i;
+    echo "\n";
   }
 }
-*/
+
+echo "\n";
